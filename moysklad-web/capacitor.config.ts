0d00d26960e26984b89e8f486c388a7c84f1c8e6
wik <telya2282@gmail.com>
2026-02-11 15:03:39 +0500

    chore: init capacitor android project

---
+++ moysklad-web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.velveto.app',
+  appName: 'Velveto',
+  webDir: 'public'
+};
+
+export default config;
